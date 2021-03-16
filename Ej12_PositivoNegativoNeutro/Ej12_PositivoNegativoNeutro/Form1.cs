14b58b0847e263d44ab58625540a879fe356ddc8
jaredtovar4172 <80796166+jaredtovar4172@users.noreply.github.com>
2021-03-16 17:52:50 -0600

    Add files via upload

---
+++ Ej12_PositivoNegativoNeutro/Ej12_PositivoNegativoNeutro/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej12_PositivoNegativoNeutro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = 0;
+            n = Convert.ToInt32(textBox1.Text);
+            if (n==0)
+            {
+                textBox2.Text = "Es neutro";
+            }
+            if (n < 0)
+            {
+                textBox2.Text = "Es negativo";
+            }
+            if (n > 0)
+            {
+                textBox2.Text = "Es positivo";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult salir;
+            salir = MessageBox.Show("Deseas salir de la aplicacion?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (salir == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
